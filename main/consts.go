469d99168b18a6a5aef0651ad234f335116e8c1f
Adam J. Richardson <fatman.uk@gmail.com>
2026-07-10 06:59:08 +0100

    Move consts

--- main/consts.go
+++ main/consts.go
@@ -1,1 +1,15 @@
 package main
+
+const metadataString = `Mod metadata:
+Song Title:      {{ .Title }}
+Format:          ProTracker-compatible MOD
+Channels:        4
+Speed:           {{ .Speed }}
+BPM:             {{ .BPM }}
+Pattern length:  64 rows (Len. 0x40h)
+Unique patterns: {{ .Patterns }}
+Order length:    {{ .OrderLen }}`
+
+const MOD_TOO_BIG_BYTES = (50 * 1024)
+
+const MSG_WRN_TOO_BIG = "Warning: Mod is %d bytes. Typically, they're smaller than %d bytes. This is a very large mod."
